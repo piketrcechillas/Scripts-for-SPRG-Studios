ee6c1ab54913d3d8f0a89d8a5c8ca80b90735063
Piketr Cechillas <ngocphuongpro98@gmail.com>
2020-06-26 13:45:03 +0700

    Update

--- PIKE_SpeedBasedCombat/PIKE_SpeedBasedCombat.js
+++ PIKE_SpeedBasedCombat/PIKE_SpeedBasedCombat.js
@@ -7,6 +7,15 @@
 //If an unit moves, it loses 80 CT when it ends its turn, otherwise it only loses 60 CT.
 //Also, put the material folder into the project for the gauge bar
 
+//Animation and States:
+//You can set custom Effect animations when starting unit's turn, as well as custom state to indicate
+//current starting unit. 
+//Just put
+//  {stateID:<number>, redID:<number>, blueID:<number>, greenID:<number>} to global parameters, whereas <number>
+//represents the ID of the data you want to play.
+//NOTICE! Animation position is carefully crafted, so you might need to experiment with your animation size and location
+//a little bit to get to the exact animation. I've created sample .sani Effect animation sfiles, import them and see how it works
+
 
 UnitWaitFlowEntry._completeMemberData = function(playerTurn) {
 		var event;
@@ -27,6 +36,10 @@ UnitWaitFlowEntry._completeMemberData = function(playerTurn) {
 
 		root.log(unit.getName() + " just finished their turn! " + str)
 		root.log( " Before CT:" + prevCT + "/After CT:" + unit.custom.ct)
+
+		SpeedGovernor.setState(unitTurn, IncreaseType.DECREASE);
+
+
 		unit.setMostResentMov(0);
 		
 		// Unless it's unlimited action, then wait.
@@ -41,8 +54,7 @@ UnitWaitFlowEntry._completeMemberData = function(playerTurn) {
 		}
 
 		SpeedGovernor.increaseCT();
-		var list = root.getBaseData().getSkillList();
-		var reco = list.getDataFromId(27);
+		
 		return this._capsuleEvent.enterCapsuleEvent(event, true);
 	}
 
@@ -53,6 +65,9 @@ UnitWaitFlowEntry._completeMemberData = function(playerTurn) {
 		
 		if (isWaitOnly) {
 			unit.custom.ct -= 60;
+
+			SpeedGovernor.setState(unitTurn, IncreaseType.DECREASE);
+
 			return this._buildEmptyAction();
 		}
 		else {
@@ -60,6 +75,9 @@ UnitWaitFlowEntry._completeMemberData = function(playerTurn) {
 			combination = CombinationManager.getWaitCombination(unit);
 			if (combination === null) {
 				unit.custom.ct -= 60;
+
+				SpeedGovernor.setState(unitTurn, IncreaseType.DECREASE);
+
 				root.log("pass this")
 				// Do nothing because it cannot attack.
 				return this._buildEmptyAction();
@@ -75,6 +93,15 @@ UnitWaitFlowEntry._completeMemberData = function(playerTurn) {
 
 
 TurnChangeMapStart.doLastAction = function() {
+		var stateID = root.getMetaSession().global.stateID;
+		var red = root.getMetaSession().global.redID;
+		var blue = root.getMetaSession().global.blueID;
+		var green = root.getMetaSession().global.greenID;
+
+		SpeedGovernor._setID(stateID, red, blue, green);
+
+
+
 		root.log("checkpoint 1")
 
 		unitTurn = SpeedGovernor._checkNextTurn();
@@ -84,6 +111,7 @@ TurnChangeMapStart.doLastAction = function() {
 		}
 		root.log("checkpoint 2")
 
+		SpeedGovernor.setState(unitTurn, IncreaseType.INCREASE);
 
 		if(unitTurn.getUnitType() == UnitType.PLAYER){
 				nextTurnType = TurnType.PLAYER;
@@ -91,6 +119,11 @@ TurnChangeMapStart.doLastAction = function() {
 		else {
 				nextTurnType = TurnType.ENEMY;
 			}
+
+		
+		//SpeedGovernor.playAnimation(unitTurn);
+
+
 		SpeedGovernor.setWaitAll(nextTurnType, unitTurn)
 		root.getCurrentSession().setTurnType(nextTurnType);
 		root.getCurrentSession().setTurnCount(0);
@@ -118,6 +151,10 @@ TurnChangeEnd._startNextTurn = function() {
 		else {
 			nextTurnType = TurnType.ENEMY;
 		}
+
+		SpeedGovernor.playAnimation(unitTurn);
+		SpeedGovernor.setState(unitTurn, IncreaseType.INCREASE);
+
 		SpeedGovernor.setWaitAll(nextTurnType, unitTurn)
 		root.getCurrentSession().setTurnType(nextTurnType);
 		
@@ -126,6 +163,23 @@ TurnChangeEnd._startNextTurn = function() {
 
 
 SpeedGovernor = {
+
+	_stateID: -1,
+	_animeIDRed: -1,
+	_animeIDBlue: -1,
+	_animeIDGreen: -1,
+
+	_setID: function(stateID, animeIDRed, animeIDBlue, animeIDGreen) {
+		if(stateID != null)
+			this._stateID = stateID;
+		if(animeIDRed != null)
+			this._animeIDRed = animeIDRed;
+		if(animeIDBlue != null)
+			this._animeIDBlue = animeIDBlue;
+		if(animeIDGreen != null)
+			this._animeIDGreen = animeIDGreen;
+	},
+
 	_checkNextTurn: function() {
 	var list = EnemyList.getAliveList();
 	var count = list.getCount();
@@ -273,6 +327,37 @@ SpeedGovernor = {
 				unit.setWait(false);
 			}
 		}
+	},
+
+	playAnimation: function(unitTurn) {
+		generator = root.getEventGenerator();
+		var list = root.getBaseData().getEffectAnimationList(false);
+		var anime = null;
+		if(unitTurn.getUnitType() == UnitType.PLAYER && this._animeIDBlue != -1)
+			anime = list.getDataFromId(this._animeIDBlue);
+		else if(unitTurn.getUnitType() == UnitType.ENEMY && this._animeIDRed != -1)
+			anime = list.getDataFromId(this._animeIDRed);
+		else if(unitTurn.getUnitType() == UnitType.ALLY && this._animeIDGreen != -1)
+			anime = list.getDataFromId(this._animeIDGreen);
+
+		if(anime!= null) {
+			var x = LayoutControl.getPixelX(unitTurn.getMapX());
+			var y = LayoutControl.getPixelY(unitTurn.getMapY());
+			var pos = LayoutControl.getMapAnimationPos(x, y, anime);
+			generator.animationPlay(anime, x, y, false, AnimePlayType.SYNC, 9999)
+			generator.execute();
+		}
+	},
+
+	setState: function(unit, increaseType) {
+		if(this._stateID != -1) {
+			var list = root.getBaseData().getStateList();
+			var act = list.getDataFromId(this._stateID);
+
+			StateControl.arrangeState(unit, act, increaseType);
+
+		}
+
 	}
 }
 
@@ -331,3 +416,7 @@ UnitMenuTopWindow.drawWindowContent = function(x, y) {
 		this._drawUnitCt(x, y);
 	}
 
+
+TurnMarkFlowEntry.drawFlowEntry = function() {
+
+}
